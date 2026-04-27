1911d0b3c32058f4894c1add96e3e6adf46f3e5b
Troy <troyyoymonterooo@gmail.com>
2026-04-27 09:46:00 +0800

    fix dj-database-url config error

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -77,9 +77,7 @@ WSGI_APPLICATION = 'gym_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    'default': dj_database_url.config(
         conn_max_age=600,
         ssl_require=True
     )
